cea57fb4e8bd5f5e48bd44bee2e5b5e95fdd068f
ChrisMak <cmakride@gmail.com>
2022-06-29 12:04:25 -0400

    Banner mobile and tablet basic styling

--- src/pages/home/components/banner/Banner.jsx
+++ src/pages/home/components/banner/Banner.jsx
@@ -4,8 +4,8 @@ const Banner = () => {
   return (
     <>
       <div className="w-full h-96 bg-banner bg-no-repeat bg-center bg-cover border-t-8 border-row1-2">
-        <div className="flex flex-col justify-center items-center gap-5 h-full">
-          <p className='text-center font-RobotoSlab font-light tracking-wide text-row4-7 text-3xl'>
+        <div className="flex flex-col justify-center items-center gap-8 h-full">
+          <p className='text-center font-RobotoSlab font-light tracking-wide text-row4-7 text-xs md:text-2xl lg:text-3xl'>
             Providing independent research, analysis, and<br />
             resources to facilitate informed policy decisions<br />
             and administration of services
--- src/pages/home/components/banner/components/BannerSearchBar.jsx
+++ src/pages/home/components/banner/components/BannerSearchBar.jsx
@@ -3,11 +3,11 @@ import { FaSearch } from 'react-icons/fa'
 const BannerSearchBar = () => {
   return (
     <form className="w-full flex justify-center max-w-screen-2xl">
-      <div id="search-container" className="w-1/2">
+      <div id="search-container" className="w-11/12 md:w-1/2">
 
-        <input type="search" id="default-search" className="p-1 pl-1 md:p-2 md:pl-5 text-md text-gray-900 bg-white placeholder:text-row4-3 placeholder:font-OpenSans placeholder:italic" placeholder="Search rfa.sc.gov" required />
-        <button type="submit" className="text-white font-OpenSans bg-row1-5 hover:bg-row1-3 hover:border font-regular tracking-widest flex justify-center items-center gap-1">
-          SEARCH <FaSearch className='text-xl'/>
+        <input type="search" id="default-search" className="p-2 pl-3 text-md text-gray-900 bg-white placeholder:text-row4-3 placeholder:font-OpenSans placeholder:italic" placeholder="Search rfa.sc.gov" required />
+        <button type="submit" className="text-white text-sm font-OpenSans bg-row1-5 hover:bg-row1-3 hover:border font-regular tracking-widest flex justify-center items-center gap-1">
+          SEARCH <FaSearch className='text-md md:text-xl'/>
         </button>
 
         <style>{`
@@ -16,6 +16,11 @@ const BannerSearchBar = () => {
       grid-template-columns: 1.4fr 0.6fr;; 
       grid-template-rows: 1fr; 
       gap: 6px; 
+      }
+      @media screen and (max-width:640px){
+        #search-container{
+          gap: 0px; 
+        }
       }
       `}
         </style>
