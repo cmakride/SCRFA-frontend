418867aa6d841a37dca14e03f05928e6a1c05317
ChrisMak <cmakride@gmail.com>
2022-06-29 11:21:54 -0400

    BannerSearchBar component

--- src/pages/home/components/banner/Banner.jsx
+++ src/pages/home/components/banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <>
       <div className="w-full h-96 bg-banner bg-no-repeat bg-center bg-cover border-t-8 border-row1-2">
-        <div className="flex flex-col justify-center items-center h-full">
+        <div className="flex flex-col justify-center items-center gap-5 h-full">
           <p className='text-center font-RobotoSlab font-light tracking-wide text-row4-7 text-3xl'>
             Providing independent research, analysis, and<br />
             resources to facilitate informed policy decisions<br />
--- src/pages/home/components/banner/components/BannerSearchBar.jsx
+++ src/pages/home/components/banner/components/BannerSearchBar.jsx
@@ -1,9 +1,27 @@
+import { FaSearch } from 'react-icons/fa'
+
 const BannerSearchBar = () => {
-  return ( 
-    <>
-    <h1>SANITY</h1>
-    </>
-   );
+  return (
+    <form className="w-full flex justify-center max-w-screen-2xl">
+      <div id="search-container" className="w-1/2">
+
+        <input type="search" id="default-search" className="p-1 pl-1 md:p-2 md:pl-5 text-md text-gray-900 bg-white placeholder:text-row4-3 placeholder:font-OpenSans placeholder:italic" placeholder="Search rfa.sc.gov" required />
+        <button type="submit" className="text-white font-OpenSans bg-row1-5 hover:bg-row1-3 hover:border font-regular tracking-widest flex justify-center items-center gap-1">
+          SEARCH <FaSearch className='text-xl'/>
+        </button>
+
+        <style>{`
+    #search-container{
+      display: grid; 
+      grid-template-columns: 1.4fr 0.6fr;; 
+      grid-template-rows: 1fr; 
+      gap: 6px; 
+      }
+      `}
+        </style>
+      </div>
+    </form>
+  );
 }
- 
+
 export default BannerSearchBar;
